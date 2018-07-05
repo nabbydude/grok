608d1d423600bfb7bceb6a4991ec583f2a594f32
Riley Froude <nabbydude@gmail.com>
2018-07-05 02:08:22 -0400

    added array escaping to potentially dangerous inputs

--- src/branches/card-name.ts
+++ src/branches/card-name.ts
@@ -5,5 +5,7 @@ import { CardName } from "@/types/general";
 import { CARD_NAMES } from "@/data";
 
 export const parseCardName = r.anyOf(...CARD_NAMES.map(
-  cardname => r.reg(new RegExp(cardname)).as(_ => <CardName>cardname)
+  cardname => r.reg(
+    new RegExp(cardname.replace(/[-/\\^$*+?.()|[\]{}]/g, "\\$&"))
+  ).as(_ => <CardName>cardname)
 ));
--- src/branches/counter-name.ts
+++ src/branches/counter-name.ts
@@ -5,5 +5,7 @@ import { CounterName } from "@/types/general";
 import { COUNTERS } from "@/data";
 
 export const parseCounterName = r.anyOf(...COUNTERS.map(
-  counter => r.reg(new RegExp(counter, "i")).as(_ => <CounterName>counter)
+  counter => r.reg(
+    new RegExp(counter.replace(/[-/\\^$*+?.()|[\]{}]/g, "\\$&"), "i")
+  ).as(_ => <CounterName>counter)
 ));
