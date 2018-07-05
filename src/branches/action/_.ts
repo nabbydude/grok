52592c88afeda1c7321d83a9e2f5032a74a49e12
Riley Froude <nabbydude@gmail.com>
2018-07-05 02:13:44 -0400

    fixed formatting

--- src/branches/action/_.ts
+++ src/branches/action/_.ts
@@ -32,6 +32,5 @@ export const parseAction = r.anyOf<Action>(
   parseProliferateAction,
   parseScryAction,
   parseSupportAction,
-  
   parseScopedAction
 );
--- src/branches/action/bolster.ts
+++ src/branches/action/bolster.ts
@@ -4,4 +4,6 @@ import { BolsterAction } from "@/types/action";
 
 import { parseValue } from "@/branches/value/_";
 
-export const parseBolsterAction = r`bolsters? ${parseValue}`.as(([value]) => <BolsterAction>{ type: "bolster", value });
+export const parseBolsterAction = r`bolsters? ${parseValue}`.as(
+  ([value]) => <BolsterAction>{ type: "bolster", value }
+);
